1b1af7cfaef7ed77bb8ff6aafcba0dd53246bf58
Kristian Binau <kristian@binau.me>
2022-05-18 21:56:26 +0200

    Add proxy

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,8 +42,11 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/'
+    proxy: true,
+  },
+
+  proxy: {
+    '/api/': { target: 'https://restaurant-backend.binau.dev/', pathRewrite: {'^/api/': ''}, changeOrigin: true }
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
